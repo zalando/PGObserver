4ea2d228a4d7a88e00ae9355e9e437cd45b7e852
Jan Mußler <jan.mussler@zalando.de>
2013-10-21 17:10:15 +0200

    Fixed bug with functions with no paramters

--- gatherer/src/main/java/de/zalando/pgobserver/gatherer/SprocIdCache.java
+++ gatherer/src/main/java/de/zalando/pgobserver/gatherer/SprocIdCache.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,14 +13,12 @@ import java.util.TreeMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
- * @author  jmussler
+ * @author jmussler
  */
 public class SprocIdCache {
-    
-    private static final Logger LOG = LoggerFactory.getLogger(SprocIdCache.class);
 
+    private static final Logger LOG = LoggerFactory.getLogger(SprocIdCache.class);
     protected Host host;
     protected Map<String, Map<String, Integer>> cache = null;
 
@@ -28,12 +27,12 @@ public class SprocIdCache {
         cache = new TreeMap<String, Map<String, Integer>>();
     }
 
-    public int getId(final Connection conn, SprocPerfValue v ) throws SQLException {
+    public int getId(final Connection conn, SprocPerfValue v) throws SQLException {
 
         Map<String, Integer> m = cache.get(v.schema);
         if (m == null) {
             Map<String, Integer> newM = new HashMap<String, Integer>();
-            cache.put(v.schema,newM);
+            cache.put(v.schema, newM);
             m = newM;
         }
 
@@ -47,10 +46,19 @@ public class SprocIdCache {
     }
 
     public static String getFunctionName(SprocPerfValue v) {
-        List<String> types = Splitter.on(',').splitToList(v.parameters);
-        List<String> modes = Splitter.on(',').splitToList(v.parameterModes);
+        List<String> types;
+        List<String> modes;
 
-        if(types.size()!=modes.size()) {
+        if (v.parameters == null || "".equals(v.parameters)
+                || v.parameterModes == null || "".equals(v.parameterModes)) {
+            types = new ArrayList<>(0);
+            modes = new ArrayList<>(0);
+        } else {
+            types = Splitter.on(',').splitToList(v.parameters);
+            modes = Splitter.on(',').splitToList(v.parameterModes);
+        }
+
+        if (types.size() != modes.size()) {
             LOG.error("List of Types and List of Modes is not equal in size");
             return v.name;
         }
@@ -58,15 +66,14 @@ public class SprocIdCache {
         StringBuilder b = new StringBuilder();
         b.append(v.name).append("(");
         int i = 0;
-        for(String t : types) {
-            if(i>0) {
+        for (String t : types) {
+            if (i > 0) {
                 b.append(", ");
             }
 
-            if(modes.get(i).equals("o")) {
+            if (modes.get(i).equals("o")) {
                 b.append(t);
-            }
-            else {
+            } else {
                 b.append("i ").append(t);
             }
             i++;
@@ -77,15 +84,29 @@ public class SprocIdCache {
 
     /**
      * generate the old function name, no distinction for in/out parameters
-     * 
+     *
      * @param v
-     * @return 
+     * @return
      */
     public static String getOldFunctionName(SprocPerfValue v) {
-        List<String> types = Splitter.on(',').splitToList(v.parameters);
-        List<String> modes = Splitter.on(',').splitToList(v.parameterModes);
+        List<String> types;
+        List<String> modes;
 
-        if(types.size()!=modes.size()) {
+        if (v.parameters == null || "".equals(v.parameters)
+                || v.parameterModes == null || "".equals(v.parameterModes)) {
+            types = new ArrayList<>(0);
+            modes = new ArrayList<>(0);
+        } else {
+            types = Splitter.on(',').splitToList(v.parameters);
+            modes = Splitter.on(',').splitToList(v.parameterModes);
+        }
+
+        if (types.size() != modes.size()) {
+            LOG.error("List of Types and List of Modes is not equal in size");
+            return v.name;
+        }
+
+        if (types.size() != modes.size()) {
             LOG.error("List of Types and List of Modes is not equal in size");
             return v.name;
         }
@@ -93,8 +114,8 @@ public class SprocIdCache {
         StringBuilder b = new StringBuilder();
         b.append(v.name).append("(");
         int i = 0;
-        for(String t : types) {
-            if(i>0) {
+        for (String t : types) {
+            if (i > 0) {
                 b.append(", ");
             }
             b.append(t);
@@ -108,11 +129,11 @@ public class SprocIdCache {
     private int getIdForName(final Connection conn, final String schema, final String name) throws SQLException {
 
         Statement s = conn.createStatement();
-        ResultSet rs = s.executeQuery("SELECT sproc_id FROM monitor_data.sprocs " +
-                                       "WHERE sproc_schema = '" + schema + "'" +
-                                         "AND sproc_name = '" + name + "'" +
-                                         "AND sproc_host_id = " + host.id);
-        if(rs.next()) {
+        ResultSet rs = s.executeQuery("SELECT sproc_id FROM monitor_data.sprocs "
+                + "WHERE sproc_schema = '" + schema + "'"
+                + "AND sproc_name = '" + name + "'"
+                + "AND sproc_host_id = " + host.id);
+        if (rs.next()) {
             int id = rs.getInt("sproc_id");
             rs.close();
             s.close();
@@ -123,14 +144,14 @@ public class SprocIdCache {
 
     private int getCountForName(final Connection conn, final String schema, final String name) throws SQLException {
         Statement s = conn.createStatement();
-        ResultSet rs = s.executeQuery("SELECT count(1) AS count " +
-                                        "FROM monitor_data.sprocs " +
-                                       "WHERE sproc_schema = '" + schema + "'" +
-                                         "AND sproc_name = '" + name + "'" +
-                                         "AND sproc_host_id = " + host.id);
+        ResultSet rs = s.executeQuery("SELECT count(1) AS count "
+                + "FROM monitor_data.sprocs "
+                + "WHERE sproc_schema = '" + schema + "'"
+                + "AND sproc_name = '" + name + "'"
+                + "AND sproc_host_id = " + host.id);
 
         int count = 0;
-        if(rs.next()) {
+        if (rs.next()) {
             count = rs.getInt("count");
         }
         rs.close();
@@ -138,74 +159,76 @@ public class SprocIdCache {
         return count;
 
     }
-    
+
     /**
-     * Try to rewrite all function names to proper new names, also accross multiple schemas ignoring API Version
-     * 
+     * Try to rewrite all function names to proper new names, also accross
+     * multiple schemas ignoring API Version
+     *
      * @param conn
      * @param oldName
      * @param name
      * @param schema
-     * @throws SQLException 
+     * @throws SQLException
      */
     private void renameFunction(final Connection conn, final String oldName, final String name, final String schema) throws SQLException {
         Statement s = conn.createStatement();
         s.execute("UPDATE monitor_data.sprocs "
-                +    "SET sproc_name = '" + name + "' "
-                +  "WHERE sproc_name = '"+ oldName +"' "
-                +    "AND (substring(sproc_schema from '(.*)_r') = substring('"+schema+"' from '(.*)_r') OR sproc_schema = '"+schema+"') "
-                +    "AND sproc_host_id = "+host.id);
+                + "SET sproc_name = '" + name + "' "
+                + "WHERE sproc_name = '" + oldName + "' "
+                + "AND (substring(sproc_schema from '(.*)_r') = substring('" + schema + "' from '(.*)_r') OR sproc_schema = '" + schema + "') "
+                + "AND sproc_host_id = " + host.id);
         s.close();
     }
-        
+
     private int createNewFunctionEntry(final Connection conn, final String schema, final String name) throws SQLException {
         Statement s = conn.createStatement();
         ResultSet rs = s.executeQuery(
-                    "INSERT INTO monitor_data.sprocs ( sproc_host_id, sproc_schema, sproc_name ) "
-                    + "VALUES (" + host.id + ", '" + schema + "','" + name + "') RETURNING sproc_id");
-        
-        if(rs.next()) {
+                "INSERT INTO monitor_data.sprocs ( sproc_host_id, sproc_schema, sproc_name ) "
+                + "VALUES (" + host.id + ", '" + schema + "','" + name + "') RETURNING sproc_id");
+
+        if (rs.next()) {
             int id = rs.getInt(1);
             rs.close();
             s.close();
             return id;
         }
-        
+
         return 0;
     }
 
     /**
-     * If no function is found, either
-     *  - create a new entry
-     *  - rename old entry to new name if function name is unique ( then it is probably the same function )
-     * 
+     * If no function is found, either - create a new entry - rename old entry
+     * to new name if function name is unique ( then it is probably the same
+     * function )
+     *
      * @param conn
      * @param v
      * @return
-     * @throws SQLException 
+     * @throws SQLException
      */
     public int newValue(final Connection conn, SprocPerfValue v) throws SQLException {
         String name = getFunctionName(v);
-        int id = getIdForName(conn,v.schema, name);
-        
-        if(id==0) {
+        int id = getIdForName(conn, v.schema, name);
+
+        if (id == 0) {
             String oldName = getOldFunctionName(v);
-            
-            int idOld = getIdForName(conn,v.schema,oldName);            
-            if(idOld!=0 && v.collisions==1) {
-                int count = getCountForName(conn,v.schema,oldName);
-                if(count==1) {
+
+            int idOld = getIdForName(conn, v.schema, oldName);
+            if (idOld != 0 && v.collisions == 1) {
+                int count = getCountForName(conn, v.schema, oldName);
+                if (count == 1) {
                     // update function name to new name, because no collision and realy only one function in pgobserver                    
-                    renameFunction(conn, oldName, name , v.schema);
+                    renameFunction(conn, oldName, name, v.schema);
                     return idOld;
                 }
             }
-            
+
             /**
-             * There is more than one matching or a collision on the observed schema, so create a new function here
+             * There is more than one matching or a collision on the observed
+             * schema, so create a new function here
              */
             id = createNewFunctionEntry(conn, v.schema, name);
-        }        
+        }
         return id;
     }
 }
--- gatherer/src/test/java/de/zalando/pgobserver/gatherer/SprocIdCacheTest.java
+++ gatherer/src/test/java/de/zalando/pgobserver/gatherer/SprocIdCacheTest.java
@@ -11,7 +11,6 @@ public class SprocIdCacheTest {
 
     @Test
     public void testGetFunctionName() {
-        System.out.println("getFunctionName");
         SprocPerfValue v = new SprocPerfValue();
         v.name = "some_function";
         v.parameters = "integer,integer,character varying";
@@ -23,12 +22,33 @@ public class SprocIdCacheTest {
 
     @Test
     public void testGetOldFunctionName() {
-        System.out.println("getFunctionName");
         SprocPerfValue v = new SprocPerfValue();
         v.name = "some_function";
         v.parameters = "integer,integer,character varying";
         v.parameterModes = "i,o,o";
         String expResult = "some_function(integer, integer, character varying)";
+        String result = SprocIdCache.getOldFunctionName(v);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testGetNewFunctionNameNoParams() {
+        SprocPerfValue v = new SprocPerfValue();
+        v.name = "some_function";
+        v.parameters = "";
+        v.parameterModes = "";
+        String expResult = "some_function()";
+        String result = SprocIdCache.getFunctionName(v);
+        assertEquals(expResult, result);
+    }
+    
+        @Test
+    public void testGetOldFunctionNameNoParams() {
+        SprocPerfValue v = new SprocPerfValue();
+        v.name = "some_function";
+        v.parameters = "";
+        v.parameterModes = "";
+        String expResult = "some_function()";
         String result = SprocIdCache.getOldFunctionName(v);
         assertEquals(expResult, result);
     }
