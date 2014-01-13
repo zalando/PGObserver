3415f308f1500c06417872121ac35383e0ea0911
Igor Ramadas <igor@devv.com>
2014-01-13 16:02:11 +0100

    PF-2623, PF-2621, PF-2622

--- frontend/static/js/flot/jquery.flot.autoscale.js
+++ frontend/static/js/flot/jquery.flot.autoscale.js
@@ -2,20 +2,23 @@ function flotSmartRescale(data, from, to) {
 
     // Trim the dataseries, but add a small extra to min and max to avoid clipping.
     var rescaledSeries = [];
-    from -= 1;
-    to += 1;
 
-    // Iterate data.
+    // Iterate data. Add an extra minute to the timespan to avoid clipping.
     $.each(data, function(e, obj){
         var newData = [];
+        from -= 60000;
+        to += 60000;
 
-        $.each(obj["data"], function(e, val){
+        $.each(obj.data, function(e, val){
             if ((val[0] >= from) && (val[0] <= to)) {
                 newData.push(val);
             }
         });
 
-        rescaledSeries.push({label: obj.label, color: obj.color, data: newData});
+        var newSerie = {label: obj.label, color: obj.color, data: newData};
+        if (obj.yaxis) newSerie.yaxis = obj.yaxis;
+
+        rescaledSeries.push(newSerie);
     });
 
     return rescaledSeries;
