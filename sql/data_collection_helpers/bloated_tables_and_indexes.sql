ac3de84e71d6593f8e64f68a4b5eaad9ceb85803
kmoppel <kaarel.moppel@zalando.de>
2014-11-07 16:30:34 +0100

    "bloat queries" updated to use queries from https://github.com/OPMDG/check_pgactivity

--- sql/data_collection_helpers/bloated_tables_and_indexes.sql
+++ sql/data_collection_helpers/bloated_tables_and_indexes.sql
@@ -22,63 +22,68 @@ DECLARE
      RETURNS SETOF record AS
     $$
     SELECT
-      *,
-      pg_size_pretty(wasted_bytes::int8) AS wasted_bytes_pretty
+      schemaname::text,
+      tblname::text,
+      bloat_ratio::numeric,
+      bloat_size::numeric,
+      pg_size_pretty(bloat_size::int8)
     FROM (
-    SELECT
-      schemaname::text AS schema_name,
-      tablename::text AS table_name,
-      ROUND(CASE WHEN otta=0 THEN 0.0 ELSE sml.relpages/otta::numeric END,1) AS bloat_factor,
-      CASE WHEN relpages < otta THEN 0 ELSE bs*(sml.relpages-otta)::bigint END AS wasted_bytes
-    FROM
-      (
+        SELECT current_database(), schemaname, tblname, bs*tblpages AS real_size,
+        (tblpages-est_num_pages)*bs AS bloat_size,
+        CASE WHEN tblpages - est_num_pages > 0
+        THEN 100 * (tblpages - est_num_pages)/tblpages::float
+        ELSE 0
+        END AS bloat_ratio
+        FROM (
+        SELECT ceil(reltuples/((bs-page_hdr)/tpl_size)) + ceil(toasttuples/4) AS est_num_pages,
+        tblpages, bs, tblid, schemaname, tblname, heappages, toastpages, is_na
+        FROM (
         SELECT
-          schemaname,
-          tablename,
-          cc.relpages,
-          bs,
-          CEIL((cc.reltuples*((datahdr+ma-
-            (CASE WHEN datahdr%ma=0 THEN ma ELSE datahdr%ma END))+nullhdr2+4))/(bs-20::float))
-            AS otta
-        FROM
-          (
-              SELECT
-                ma,bs,schemaname,tablename,
-                (datawidth+(hdr+ma-(case when hdr%ma=0 THEN ma ELSE hdr%ma END)))::numeric AS datahdr,
-                (maxfracsum*(nullhdr+ma-(case when nullhdr%ma=0 THEN ma ELSE nullhdr%ma END))) AS nullhdr2
-              FROM
-                (
-                  SELECT
-                    schemaname, tablename, hdr, ma, bs,
-                    SUM((1-null_frac)*avg_width) AS datawidth,
-                    MAX(null_frac) AS maxfracsum,
-                    hdr+(
-                        SELECT 1+count(*)/8
-                        FROM pg_stats s2
-                        WHERE null_frac<>0 AND s2.schemaname = s.schemaname AND s2.tablename = s.tablename
-                      ) AS nullhdr
-                  FROM
-                    pg_stats s,
-                    (
-                      SELECT
-                        (
-                          SELECT current_setting('block_size')::numeric) AS bs,
-                          CASE WHEN substring(v,12,3) IN ('8.0','8.1','8.2') THEN 27 ELSE 23 END AS hdr,
-                          CASE WHEN v ~ 'mingw32' THEN 8 ELSE 4 END AS ma
-                      FROM (SELECT version() AS v) AS foo
-                    ) AS constants
-                  GROUP BY
-                    1,2,3,4,5
-              ) AS foo
-          ) AS rs
-          JOIN
-          pg_class cc ON cc.relname = rs.tablename
-          JOIN
-          pg_namespace nn ON cc.relnamespace = nn.oid AND nn.nspname = rs.schemaname AND nn.nspname <> 'information_schema'
-      ) AS sml
-    ) t
+        ( 4 + tpl_hdr_size + tpl_data_size + (2*ma)
+        - CASE WHEN tpl_hdr_size%ma = 0 THEN ma ELSE tpl_hdr_size%ma END
+        - CASE WHEN ceil(tpl_data_size)::int%ma = 0 THEN ma ELSE ceil(tpl_data_size)::int%ma END
+        ) AS tpl_size, bs - page_hdr AS size_per_block, (heappages + toastpages) AS tblpages, heappages,
+        toastpages, reltuples, toasttuples, bs, page_hdr, tblid, schemaname, tblname, is_na
+        FROM (
+        SELECT
+        tbl.oid AS tblid, ns.nspname AS schemaname, tbl.relname AS tblname, tbl.reltuples,
+        tbl.relpages AS heappages, coalesce(toast.relpages, 0) AS toastpages,
+        coalesce(toast.reltuples, 0) AS toasttuples,
+        current_setting('block_size')::numeric AS bs,
+        CASE WHEN version()~'mingw32' OR version()~'64-bit|x86_64|ppc64|ia64|amd64' THEN 8 ELSE 4 END AS ma,
+        24 AS page_hdr,
+        23 + CASE WHEN MAX(coalesce(null_frac,0)) > 0 THEN ( 7 + count(*) ) / 8 ELSE 0::int END
+        + CASE WHEN tbl.relhasoids THEN 4 ELSE 0 END AS tpl_hdr_size,
+        sum( (1-coalesce(s.null_frac, 0))
+        * coalesce(
+        CASE
+        WHEN t.typlen = -1 THEN
+        CASE WHEN s.avg_width < 127
+        THEN s.avg_width + 1 ELSE s.avg_width + 4
+        END
+        WHEN t.typlen = -2 THEN s.avg_width + 1
+        ELSE t.typlen
+        END
+        , 1024)) AS tpl_data_size,
+        bool_or(att.atttypid = 'pg_catalog.name'::regtype) AS is_na
+        FROM pg_attribute AS att
+        JOIN pg_type AS t ON att.atttypid = t.oid
+        JOIN pg_class AS tbl ON att.attrelid = tbl.oid
+        JOIN pg_namespace AS ns ON ns.oid = tbl.relnamespace
+        JOIN pg_stats AS s ON s.schemaname=ns.nspname
+        AND s.tablename = tbl.relname AND s.inherited=false AND s.attname=att.attname
+        LEFT JOIN pg_class AS toast ON tbl.reltoastrelid = toast.oid
+        WHERE att.attnum > 0 AND NOT att.attisdropped
+        AND tbl.relkind = 'r'
+        GROUP BY 1,2,3,4,5,6,7,8,9,10, tbl.relhasoids
+        ORDER BY 2,3
+        ) AS s
+        ) AS s2
+        ) AS s3
+        WHERE NOT is_na
+    ) AS o
     ORDER BY
-      CASE WHEN p_order_by_bloat_factor THEN bloat_factor ELSE wasted_bytes END DESC
+      CASE WHEN p_order_by_bloat_factor THEN bloat_ratio ELSE bloat_size END DESC
     LIMIT
       p_limit
     $$ LANGUAGE sql VOLATILE SECURITY DEFINER;
@@ -112,66 +117,74 @@ DECLARE
      RETURNS SETOF record AS
     $$
     SELECT
-      *,
-      pg_size_pretty(wasted_bytes::int8) AS wasted_bytes_pretty
+      schemaname::text,
+      tblname::text,
+      idxname::text,
+      bloat_ratio::numeric,
+      bloat_size::numeric,
+      pg_size_pretty(bloat_size::int8)
     FROM (
+        SELECT current_database(), nspname AS schemaname, tblname, idxname, bs*(sub.relpages)::bigint AS real_size,
+        bs*est_pages::bigint as estimated_size,
+        bs*(sub.relpages-est_pages)::bigint AS bloat_size,
+        100 * (sub.relpages-est_pages)::float / sub.relpages AS bloat_ratio, is_na
+        FROM (
+        SELECT bs, nspname, table_oid, tblname, idxname, relpages, coalesce(
+        1+ceil(reltuples/floor((bs-pageopqdata-pagehdr)/(4+nulldatahdrwidth)::float)), 0
+        ) AS est_pages, is_na
+        FROM (
+        SELECT maxalign, bs, nspname, tblname, idxname, reltuples, relpages, relam, table_oid,
+        ( index_tuple_hdr_bm +
+        maxalign - CASE
+        WHEN index_tuple_hdr_bm%maxalign = 0 THEN maxalign
+        ELSE index_tuple_hdr_bm%maxalign
+        END
+        + nulldatawidth + maxalign - CASE
+        WHEN nulldatawidth = 0 THEN 0
+        WHEN nulldatawidth::integer%maxalign = 0 THEN maxalign
+        ELSE nulldatawidth::integer%maxalign
+        END
+        )::numeric AS nulldatahdrwidth, pagehdr, pageopqdata, is_na
+        FROM (
         SELECT
-          schemaname::text AS schema_name,
-          tablename::text AS table_name,
-          iname::text AS index_name,
-          ROUND(CASE WHEN iotta=0 OR ipages=0 THEN 0.0 ELSE ipages/iotta::numeric END,1) AS bloat_factor,
-          CASE WHEN ipages < iotta THEN 0 ELSE (bs*(ipages-iotta))::numeric END AS wasted_bytes
-        FROM
-          (
-            SELECT
-              schemaname,
-              tablename,
-              cc.relpages,
-              bs,
-              COALESCE(c2.relname,'?') AS iname, COALESCE(c2.reltuples,0) AS ituples, COALESCE(c2.relpages,0) AS ipages,
-              COALESCE(CEIL((c2.reltuples*(datahdr-12))/(bs-20::float)),0) AS iotta -- very rough approximation, assumes all cols
-            FROM
-              (
-                  SELECT
-                    ma,bs,schemaname,tablename,
-                    (datawidth+(hdr+ma-(case when hdr%ma=0 THEN ma ELSE hdr%ma END)))::numeric AS datahdr,
-                    (maxfracsum*(nullhdr+ma-(case when nullhdr%ma=0 THEN ma ELSE nullhdr%ma END))) AS nullhdr2
-                  FROM
-                    (
-                      SELECT
-                        schemaname, tablename, hdr, ma, bs,
-                        SUM((1-null_frac)*avg_width) AS datawidth,
-                        MAX(null_frac) AS maxfracsum,
-                        hdr+(
-                            SELECT 1+count(*)/8
-                            FROM pg_stats s2
-                            WHERE null_frac<>0 AND s2.schemaname = s.schemaname AND s2.tablename = s.tablename
-                          ) AS nullhdr
-                      FROM
-                        pg_stats s,
-                        (
-                          SELECT
-                            (SELECT current_setting('block_size')::numeric) AS bs,
-                            CASE WHEN substring(v,12,3) IN ('8.0','8.1','8.2') THEN 27 ELSE 23 END AS hdr,
-                            CASE WHEN v ~ 'mingw32' THEN 8 ELSE 4 END AS ma
-                          FROM (SELECT version() AS v) AS foo
-                        ) AS constants
-                      GROUP BY
-                        1,2,3,4,5
-                  ) AS foo
-              ) AS rs
-              JOIN
-              pg_class cc ON cc.relname = rs.tablename
-              JOIN
-              pg_namespace nn ON cc.relnamespace = nn.oid AND nn.nspname = rs.schemaname AND nn.nspname <> 'information_schema'
-              LEFT JOIN
-              pg_index i ON indrelid = cc.oid
-              LEFT JOIN
-              pg_class c2 ON c2.oid = i.indexrelid
-          ) AS sml
-    ) t
+        i.nspname, i.tblname, i.idxname, i.reltuples, i.relpages, i.relam, a.attrelid AS table_oid,
+        current_setting('block_size')::numeric AS bs,
+        CASE
+        WHEN version() ~ 'mingw32' OR version() ~ '64-bit|x86_64|ppc64|ia64|amd64' THEN 8
+        ELSE 4
+        END AS maxalign,
+        24 AS pagehdr,
+        16 AS pageopqdata,
+        CASE WHEN max(coalesce(s.null_frac,0)) = 0
+        THEN 2
+        ELSE 2 + (( 32 + 8 - 1 ) / 8)
+        END AS index_tuple_hdr_bm,
+        sum( (1-coalesce(s.null_frac, 0)) * coalesce(s.avg_width, 1024)) AS nulldatawidth,
+        max( CASE WHEN a.atttypid = 'pg_catalog.name'::regtype THEN 1 ELSE 0 END ) > 0 AS is_na
+        FROM pg_attribute AS a
+        JOIN (
+        SELECT nspname, tbl.relname AS tblname, idx.relname AS idxname, idx.reltuples, idx.relpages, idx.relam,
+        indrelid, indexrelid, indkey::smallint[] AS attnum
+        FROM pg_index
+        JOIN pg_class idx ON idx.oid=pg_index.indexrelid
+        JOIN pg_class tbl ON tbl.oid=pg_index.indrelid
+        JOIN pg_namespace ON pg_namespace.oid = idx.relnamespace
+        WHERE pg_index.indisvalid AND tbl.relkind = 'r'
+        ) AS i ON a.attrelid = i.indexrelid
+        JOIN pg_stats AS s ON s.schemaname = i.nspname
+        AND ((s.tablename = i.tblname AND s.attname = pg_catalog.pg_get_indexdef(a.attrelid, a.attnum, TRUE))
+        OR (s.tablename = i.idxname AND s.attname = a.attname))
+        JOIN pg_type AS t ON a.atttypid = t.oid
+        WHERE a.attnum > 0
+        GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
+        ) AS s1
+        ) AS s2
+        JOIN pg_am am ON s2.relam = am.oid WHERE am.amname = 'btree'
+        ) AS sub
+        WHERE NOT is_na
+    ) AS o
     ORDER BY
-      CASE WHEN p_order_by_bloat_factor THEN bloat_factor ELSE wasted_bytes END DESC
+      CASE WHEN p_order_by_bloat_factor THEN bloat_ratio ELSE bloat_size END DESC
     LIMIT
       p_limit
     $$ LANGUAGE sql VOLATILE SECURITY DEFINER;
