57a52f443285e0c132cfc2b9d62611a2c188271f
Jan Mussler <jan.mussler@zalando.de>
2014-12-19 15:17:59 +0100

    Work on Vagrant/Docker

--- gatherer/run.sh
+++ gatherer/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DATE=$(date +%F_%H%M)
-JAR=$(ls target/PGObserver-Gatherer-*-jar-with-dependencies.jar | tail -1)
+JAR=$(ls target/pgobserver-gatherer-*-jar-with-dependencies.jar | tail -1)
 
 nohup java -jar ${JAR} &> pgmon_java_${DATE}.log &
