zalando/PGObserver

commit af309f0f5a8828737f65cdbca396e3e1edad3895

Fix filter and total size

frontend/src/tabledata.py
@@ -279,11 +279,6 @@ def getTopTables(hostId, date_from, date_to, order=None, limit=10, pattern=None)
     if not order:
         order = 2   # size
 
-    if pattern!=None and pattern!="":
-      pattern_filter = " WHERE t_name ilike '\\%"+ str(adapt(pattern)) +"\\%' "
-    else:
-      pattern_filter = ''
-
     order_by_sql = { 1: "ORDER BY schema ASC,name ASC ",
               2: "ORDER BY table_size DESC" ,
               3: "ORDER BY table_size - min_table_size DESC",
@@ -370,9 +365,12 @@ def getTopTables(hostId, date_from, date_to, order=None, limit=10, pattern=None)
           JOIN
           monitor_data.tables ON t_id = q_max_sizes.tsd_table_id
         ) t
-        """ + pattern_filter + order_by_sql + limit_sql
+         WHERE name ilike %s 
+        """ + order_by_sql + limit_sql
 
-    list = datadb.execute(sql, (hostId, hostId, date_from, date_to))
+    pattern = '%' + pattern + '%'
+
+    list = datadb.execute(sql, (hostId, hostId, date_from, date_to, pattern))
     for d in list:
 
         d['table_size_pretty'] = makePrettySize( d['table_size'] )

frontend/src/tablesfrontend.py
@@ -169,8 +169,11 @@ class TableFrontend(object):
         total_size = 0
         total_index_size = 0
         for t in top_tables:
-            total_size = t['table_size']
-            total_index_size = t['index_size']
+            total_size += t['table_size']
+            total_index_size += t['index_size']
+
+        total_size = tabledata.makePrettySize(total_size)
+        total_index_size = tabledata.makePrettySize(total_index_size)
 
         return tpl.render(hostname=hostname, hostuiname=hostUiName, order=int(order), list=top_tables,
                           date_from=date_from, date_to=date_to, pattern=pattern,total_size=total_size, total_index_size=total_index_size)

frontend/src/templates/all_tables.html
@@ -48,7 +48,7 @@
               </tr>
           {% endfor %}
           <tr>
-            <td></td><td>{{ total_size }}</td><td></td><td></td><td>{{ total_index_size }}</td>
+            <td></td><td align=right>{{ total_size }}</td><td></td><td></td><td align=right>{{ total_index_size }}</td>
           </tr>
       </table>
 

commit b0140d4baec42d5e889f20ce14ecfe2823845f13

Frontend: fixed "top tables" (/tables) view bug introduced with tablename filtering in af309f0f5a8828737f65cdbca396e3e1edad3895

frontend/src/tabledata.py
@@ -291,7 +291,7 @@ def getTableData(host, name, interval = None):
     return d
 
 
-def getTopTables(hostId, date_from, date_to, order=None, limit=10, pattern=None):
+def getTopTables(hostId, date_from, date_to, order=None, limit=10, pattern=''):
     limit_sql = "" if limit is None else """ LIMIT """ + str(adapt(limit))
     if not order:
         order = 2   # size