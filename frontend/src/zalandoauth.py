6cb46bb7355841b500f756136ef9ef1f6c0ffd80
Evans Akai Bekoe <evans.akai.bekoe@zalando.de>
2015-09-15 18:19:00 +0200

    cosmetic changes

--- frontend/src/zalandoauth.py
+++ frontend/src/zalandoauth.py
@@ -8,7 +8,6 @@ from urllib import urlencode
 
 
 FLAG_access_token = 'access_token'
-random_flag = os.urandom(24)
 
 cherrypy.config.update({
     'tools.zalandoauthtool.on': True,
@@ -39,7 +38,7 @@ class ZalandOauth(cherrypy.Tool):
         auth_code = cherrypy.request.params.get('code')
         target_url = cherrypy.request.params.get('state')
         if auth_code:
-            # check the token
+            # get access token
             data = {'grant_type': 'authorization_code', 'code': auth_code
                     , 'redirect_uri': self.redirect_url}
             response = requests.post(self.access_token_url
@@ -48,14 +47,15 @@ class ZalandOauth(cherrypy.Tool):
             if response.json().get('access_token'):
                 access_token = response.json()['access_token']
                 cherrypy.session[FLAG_access_token] = access_token
+                response.close()
                 # redirect to endpoint where user attempted to access
                 if target_url:
                     raise cherrypy.HTTPRedirect(target_url)
                 else:
                     raise cherrypy.HTTPRedirect('/')
             else:
+                response.close()
                 raise Exception(response.json())
-            response.close()
         if not cherrypy.session.get(FLAG_access_token):
             target_url = cherrypy.url(qs=cherrypy.request.query_string)
             params = {'response_type':'code', 'redirect_uri': self.redirect_url
@@ -63,7 +63,7 @@ class ZalandOauth(cherrypy.Tool):
             raise cherrypy.HTTPRedirect("%s&%s" % (self.authorize_url, urlencode(params)))
         # prevent session regeneration
         # http://docs.cherrypy.org/en/latest/pkg/cherrypy.lib.html?#session-fixation-protection
-        cherrypy.session['flag'] = random_flag
+        cherrypy.session['flag'] = os.urandom(24)
 
     def callable(self):
         self.zalandoauthtool()
