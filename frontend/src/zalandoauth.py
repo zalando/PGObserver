a239e6e7535c938c3b6c4a4bf4d06abbae974ba3
Evans Akai Bekoe <evans.akai.bekoe@zalando.de>
2015-09-17 18:01:00 +0200

    logout implemented, minor improvements

--- frontend/src/zalandoauth.py
+++ frontend/src/zalandoauth.py
@@ -3,7 +3,6 @@
 import cherrypy
 import requests
 import os
-import json
 from urllib import urlencode
 
 
@@ -17,27 +16,30 @@ cherrypy.config.update({
 
 class ZalandOauth(cherrypy.Tool):
 
-    def __init__(self, settings):
+    def __init__(self, oauth2_settings):
         # cherrypy.session is not available before this _point
         self._point = 'before_handler'
         self._name = None
         self._priority = 0
-        oauth2 = settings['oauth2']
-        self.client_id = oauth2['client_id']
-        self.client_secret = oauth2['client_secret']
-        self.access_token_url = oauth2['access_token_url']
-        self.authorize_url = oauth2['authorize_url']
-        self.redirect_url = oauth2['redirect_url']
+        self.client_id = oauth2_settings['client_id']
+        self.client_secret = oauth2_settings['client_secret']
+        self.access_token_url = oauth2_settings['access_token_url']
+        self.authorize_url = oauth2_settings['authorize_url']
+        self.redirect_url = oauth2_settings['redirect_url']
 
+    @cherrypy.expose
     def logout(self):
-        cherrypy.sessions.expire()
+        cherrypy.lib.sessions.expire()
         raise cherrypy.HTTPRedirect('/')
-    logout.expose = True
 
     def zalandoauthtool(self):
+        # the following are used to identify currently flow
         auth_code = cherrypy.request.params.get('code')
         target_url = cherrypy.request.params.get('state')
-        if auth_code:
+        scope = cherrypy.request.params.get('scope')
+        error = cherrypy.request.params.get('error')
+        error_description = cherrypy.request.params.get('error_description')
+        if auth_code and scope and target_url:
             # get access token
             data = {'grant_type': 'authorization_code', 'code': auth_code
                     , 'redirect_uri': self.redirect_url}
@@ -54,8 +56,16 @@ class ZalandOauth(cherrypy.Tool):
                 else:
                     raise cherrypy.HTTPRedirect('/')
             else:
+                print response.json()
                 response.close()
-                raise Exception(response.json())
+                raise Exception('Failed to retrieved access-token from server!')
+        elif error and error_description:
+            print cherrypy.url(qs=cherrypy.request.query_string)
+            # in case of error e.g. access-denied we keep the target_url intact
+        else:
+            # initial case: remember endpoint where user attempted to access
+            target_url = cherrypy.url(qs=cherrypy.request.query_string)
+
         if not cherrypy.session.get(FLAG_access_token):
             target_url = cherrypy.url(qs=cherrypy.request.query_string)
             params = {'response_type':'code', 'redirect_uri': self.redirect_url
