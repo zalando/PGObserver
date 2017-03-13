74acea9f61caa8c6860236f5c28a58c13dc18e10
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2017-03-13 16:37:42 +0100

    Fix populating settings with password from env or config

--- frontend/src/web.py
+++ frontend/src/web.py
@@ -69,7 +69,7 @@ def main():
     settings['database']['port'] = (os.getenv('PGOBS_PORT') or settings['database'].get('port') or 5432)
     settings['database']['name'] = (os.getenv('PGOBS_DATABASE') or settings['database'].get('name'))
     settings['database']['frontend_user'] = (os.getenv('PGOBS_USER') or settings['database'].get('frontend_user'))
-    settings['database']['password'] = (os.getenv('PGOBS_PASSWORD') or settings['database'].get('frontend_password'))
+    settings['database']['frontend_password'] = (os.getenv('PGOBS_PASSWORD') or settings['database'].get('frontend_password'))
 
     if not (settings['database'].get('host') and settings['database'].get('name') and settings['database'].get('frontend_user')):
         print 'Mandatory datastore connect details missing!'
@@ -86,7 +86,7 @@ def main():
     ))
     print 'Setting connection string to ... ' + conn_string
     # finished print conn_string to the world, password can be added
-    conn_string = conn_string + ' password=' + settings['database']['password']
+    conn_string = conn_string + ' password=' + settings['database']['frontend_password']
 
     datadb.setConnectionString(conn_string)
 
